4796f922d06d0ab0a3c6635b113c711d947a15af
lavishtembhare <lavishtembhare93@gmail.com>
2026-08-24 17:56:36 +0530

    Update

--- src/components/Dashboard/CreateNewShorten.jsx
+++ src/components/Dashboard/CreateNewShorten.jsx
@@ -28,10 +28,7 @@ const CreateNewShorten = ({ setOpen, refetch }) => {
 
   // Format full shortened link using subdomain or origin
   const getFullShortUrl = (shortSlug) => {
-    const subdomain =
-      import.meta.env.VITE_REACT_SUBDOMAIN ||
-      import.meta.env.VITE_BACKEND_URL ||
-      window.location.origin
+    const subdomain =import.meta.env.VITE_BACKEND_URL || window.location.origin
     const baseUrl = subdomain.endsWith('/') ? subdomain.slice(0, -1) : subdomain
     return `${baseUrl}/${shortSlug}`
   }
